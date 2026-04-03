42e897259f0ee5e49c6af68e33feb943563d8867
Amit Das <wdbamit@gmail.com>
2026-04-03 23:09:03 +0530

    add pwa functions

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,44 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true
+      },
+      manifest: {
+        name: 'NoteNDo — Smart Task Manager',
+        short_name: 'NoteNDo',
+        description: 'Smart To-Do & Reminder App with WhatsApp Alerts',
+        theme_color: '#4F39F6',
+        background_color: '#ffffff',
+        display: 'standalone',
+        start_url: '/',
+        icons: [
+          {
+            src: '/icons/icon-192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/icons/icon-512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,png,svg}']
+      }
+    })
+  ],
 
   resolve: {
     alias: {
